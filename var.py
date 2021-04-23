a39432c0e5b81336f561bec6ec0163e2e041bac3
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-04-23 20:38:07 +0600

    Variable, String and basics.
    Code some basics to start python like variable number and string to print.

---
+++ var.py
@@ -0,0 +1,17 @@
+x = 'Hasnat'
+y = 'Osman'
+z = x + ' ' +y
+print(z)
+
+num = 11
+print(num *3)
+
+num = '12'
+print(num*3)
+
+name = 'Hasnat'
+print(name + ' is a \'good\' boy')
+
+x = 'Hasnat'
+y = '\nOsman'
+print(x+y)
