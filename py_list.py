bd8c00b12eaf7135588d742bbdc4b0ce2a1d6ba4
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-04-25 12:06:22 +0600

    List
    List uses.

---
+++ py_list.py
@@ -0,0 +1,14 @@
+name = ['Hasnat', 'Kazi', 'Santo', 'Imran']
+print(name)
+
+name.insert(4, 99)
+print(name)
+
+number = [2, 5, 55, 77]
+print(number)
+
+name.append(number)
+print(name)
+print(name[5])
+
+
