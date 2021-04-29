2ed5129589f6133b8db692c8b631be542e1c9ca7
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-04-29 14:21:36 +0600

    Add files via upload

---
+++ iter_saarc2.py
@@ -0,0 +1,8 @@
+saarc = ['Bangladesh', 'Sri Lanka', 'India', 'Pakistan', 'Nepal', 'Bhutan', 'Afganistan']
+
+country = input('Enter a country name: ')
+if country in saarc:
+    print(country, ' is a member of SAAARC!')
+else:
+    print(country, ' is not a member of SAARC')
+print('Program terminated!!')
