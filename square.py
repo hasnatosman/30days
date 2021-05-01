4e939eb99610eca2c0f45a2f967c5465b55682df
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-05-01 16:24:45 +0600

    Add files via upload

---
+++ square.py
@@ -0,0 +1,11 @@
+
+numbers = [1, 2, 3, 4, 5]
+number_square = []
+
+for number in numbers:
+    square = number ** 2
+    number_square.append(square)
+
+print('Given numbers are:', numbers)
+print('Square of given number: ', number_square)
+
